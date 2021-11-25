86d406bcc37a61220f75289be662ebe53670141a
ValeriaSG <91902764+ValeriaSG@users.noreply.github.com>
2021-11-24 21:55:53 -0600

    ExamenC.cpp
    Git bash a git hub

---
+++ ExamenC.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main ()
+{
+	printf("\n Prueba del Examen");
+	//Esta es la prueba para clonarlo para Git Hub
+	
+	printf("\ Hola Mundo :)");
+	return 0;
+}
